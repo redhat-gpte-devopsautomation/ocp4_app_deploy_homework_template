af8539aee7e2ba788b21fb43a401fe00eae88b44
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-09-04 16:15:32 -0400

    First commit for OCP 4 Homework

---
+++ bin/setup_jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Setup Jenkins Project
+if [ "$#" -ne 3 ]; then
+    echo "Usage:"
+    echo "  $0 GUID REPO CLUSTER"
+    echo "  Example: $0 wkha https://github.com/redhat-gpte-devopsautomation/advdev_homework_template.git na311.openshift.opentlc.com"
+    exit 1
+fi
+
+GUID=$1
+REPO=$2
+CLUSTER=$3
+echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
+
+# Set up Jenkins with sufficient resources
+# TBD
+
+# Create custom agent container image with skopeo
+# TBD
+
+# Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+# TBD
+
+# Make sure that Jenkins is fully up and running before proceeding!
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
+  if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
+    echo "...Yes. Jenkins is ready."
+    break
+  fi
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
